1f763272a1a03a5f939fee1a3f4210b841621690
arusf2011 <afatyga861@gmail.com>
2021-12-14 10:31:15 +0100

    Creation of an app with integration of frontend

---
+++ backend-alpha/index.js
@@ -0,0 +1,57 @@
+// Importowanie bibliotek
+const http = require('http');
+const express = require('express');
+const session = require('express-session');
+const mariadb = require('mariadb');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const path = require('path');
+// Inicjacja aplikacji
+const app = express();
+app.use(express.json())
+app.use(express.static("express"))
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine','ejs');
+app.use(session({
+    secret: 'bc03d7c13c6ecb3bc1983f2de8e99dc16a0376ee4a1ee62f48ce9fe4c79b4eac',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { expires: new Date(Date.now()+3600000), maxAge: 3600000 }
+}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+// Po inicjacji przechodzę do generowania widoków stron
+app.get('/', (req,res) => {
+    res.render('pages/index');
+    console.log('Strona index została wyświetlona');
+});
+app.get('/aktualnosci', (req,res) => {
+    res.render('pages/aktualnosci');
+    console.log('Strona aktualnosci została wyświetlona');
+});
+app.get('/cennik', (req,res) => {
+    res.render('pages/cennik');
+    console.log('Strona cennik została wyświetlona');
+});
+app.get('/kontakt', (req,res) => {
+    res.render('pages/kontakt');
+    console.log('Strona kontakt została wyświetlona');
+});
+app.get('/rezerwacje', (req,res) => {
+    res.render('pages/rezerwacje');
+    console.log('Strona rezerwacje została wyświetlona');
+});
+app.get('/serwis', (req,res) => {
+    res.render('pages/serwis');
+    console.log('Strona serwis została wyświetlona');
+});
+
+// Konfiguracja serwera
+const server = http.createServer(app);
+const port = 30362;
+/*
+ * Ustawiam na port 30362, na potrzeby wyświetlenia strony.
+ * Port może być dowolny - ważne aby stronę podłączyć pod właściwy port.
+ */
+server.listen(port);
+console.log('Serwer słucha na porcie '+port);
